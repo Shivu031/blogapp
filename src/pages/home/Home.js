93b68837d3d72d41010a464424ad747dfdaceb54
Shivu031 <128800786+Shivu031@users.noreply.github.com>
2023-08-28 10:33:19 +0530

    second commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,10 @@
-import React from 'react';
 import './home.css';
 import Header from '../../components/header/Header';
 import Posts from '../../components/posts/Posts';
 import Sidebar from '../../components/sidebar/Sidebar';
 
 const Home = () => {
+  
   return (
     <>
     <Header/>
